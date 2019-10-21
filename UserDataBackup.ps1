5d4af1211d9e535bf4946801de43541c9507c441
LMINX <30495444+LMINX@users.noreply.github.com>
2019-10-21 18:01:50 +0800

    backup destop,document,downloads

--- UserDataBackup.ps1
+++ UserDataBackup.ps1
@@ -15,6 +15,7 @@ Function Get-UserData {
         1.1 Desktop
         1.2 Document
     2.favorites from Chrome
+    3.business data : excel report 
     3.Outlook Mail file:.pst file
     4.Customre data: which should not in the build-in folder and change frequently   /ex:c:\trainning
     5.applicaiton config file including the login information /ex:configure file for wechat 
@@ -47,46 +48,58 @@ Function Get-UserData {
     )
     
 # Verify the PC is online or not
-# give the message to user when backup start and finished ***
-# give the user to choose the addition file he want to backup *****  funture fuction
-# compare the capcity of the server share to see if we can  upload the backup
-# import the task schedule to run daily
+#at the very beginning, backup all users data on PC and then do individual increatment backup for each user by ways below 
+    # give the message to user when backup start and finished ***
+    # give the user to choose the addition file he want to backup *****  funture fuction
+    # compare the capcity of the server share to see if we can  upload the backup
+    # import the task schedule to run daily
 Add-Type -AssemblyName PresentationFramework
+
+<#This module is not compatibility with win7, only for win10  
 Install-Module BurntToast -Force
 Import-Module BurntToast
-
+#>
 if ($ComputerName -ne $null){
     if( -Not (Test-Connection $ComputerName)){
-        throw ("Exception:Unable to connect the $($computername)")
+        #throw ("Exception:Unable to connect the $($computername)")
     }
 }
 
 #stare the copy process 
 $UserName=$env:USERNAME
 #need to chagne the computername parameter
-$ComputerName='RNLQ03404PC001'
+#$ComputerName='RNLQ03404PC001'
 $StoreNumber=$COMPUTERNAME.Substring(4,5)
-$StoreServer='RNLQ'+$StoreNumber+'SV001'
-$StoreShare='\\'+$StoreServer+'\StoreShared$'
+$StoreNamePrefix=$COMPUTERNAME.Substring(0,4)
+$StoreServer=$StoreNamePrefix+$StoreNumber+'SV001'
+$StoreShare='\\'+$StoreServer+'\StoreShared$'+'\BackupForWin7Upgrade\'+$UserName+'\'
 #userdate should be \\rnlq03404pc001\C$\xxx
 $UserHomeFolders='C:\Users\'+$UserName+'\'
-$UserCustomerFolder='C:\Backup\'
-$UserDataFolders=($UserHomeFolders,$UserCustomerFolder)
+$UserDesktop=$UserHomeFolders+'Desktop'
+$UserDownloads=$UserHomeFolders+'Downloads'
+$UserDocuments=$UserHomeFolders+'Documents'
+#$UserCustomerFolder='C:\Backup\'
+$UserDataFolders=($UserDesktop,$UserDownloads,$UserDocuments)
 #$UserDataFolders.Count
 $Logfile="/Log+:C:\UseDataBackup\robocopy.log"
-$UserDataFolders
+
+$wsh = New-Object -ComObject Wscript.Shell
+$wsh.Popup("Backup Start",5,"Win7Upgrade")
 #[System.Windows.MessageBox]::Show('Backup Start')
 mkdir C:\UseDataBackup
-New-BurntToastNotification -Text "Backup Start"
-$UserDataFolders.count
+
+
 foreach ($Folders in $UserDataFolders){
     #ERROR : You do not have the Backup and Restore Files user rights.
-    robocopy $Folders $StoreShare /R:1 /W:1   /TEE  $Logfile  /lev:2 #/NP /ZB
+    $Dest=$StoreShare+$Folders.Substring($UserHomeFolders.Length)
+    robocopy $Folders $Dest /R:1 /W:1   /TEE  /COPY:DAT  $Logfile  /lev:2 #/NP /ZB /CopyALL
 }
 #[System.Windows.MessageBox]::Show('Backup Ended')
-New-BurntToastNotification -Text "Backup END"
-
+#New-BurntToastNotification -Text "Backup END"
+$wsh.Popup("Backup End",5,"Win7Upgrade")
 }
 
 
-Get-UserData -ComputerName 'RNAP03833PC001'
+#Get-UserData -ComputerName $env:COMPUTERNAME
+
+Get-UserData -ComputerName rnlq03404pc001
